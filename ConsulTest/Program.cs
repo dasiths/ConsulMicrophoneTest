ca33fd37e3a54240b8601df3fab41e8cb5efd21e
Dasith@4463 <daiths@hotmail.com>
2017-01-16 11:23:43 +1100

    Added controllers

--- ConsulTest/Program.cs
+++ ConsulTest/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
+using Microphone.Core;
+using Microphone.Core.ClusterProviders;
+using Microphone.WebApi;
 
 namespace ConsulTest
 {
@@ -10,6 +14,28 @@ namespace ConsulTest
     {
         static void Main(string[] args)
         {
+            var provider = new ConsulProvider();
+            Cluster.Bootstrap(new WebApiProvider(), provider, "orders", "v1");
+            Console.ReadLine();
         }
+    }
+
+    public class OrdersController : ApiController
+    {
+        public string Get()
+        {
+            return "WebApi Service";
+        }
+
+    }
+
+    public class ServiceTestController : ApiController
+    {
+        public string Get()
+        {
+            var instance = Cluster.FindServiceInstanceAsync("orders").Result;
+            return instance.Address + instance.Port;
+        }
+
     }
 }
